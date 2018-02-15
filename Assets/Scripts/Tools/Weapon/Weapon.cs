8e7a51e9c9c44cbd35a6cd5892dc9ab417555420
Myggski <tomaswallin@hotmail.com>
2018-02-15 21:41:13 +0100

    weapon scriptable object

---
+++ Assets/Scripts/Tools/Weapon/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Weapon : ScriptableObject
+{
+    [Tooltip("Avatar of the weapon, for inventory and such.")]
+    public Sprite Avatar;
+
+    [Tooltip("Interval of the fire rate.")]
+    public FloatReference FireRate;
+
+    [Tooltip("Damage that the weapon can do, each time it fires.")]
+    public FloatReference Damage;
+
+    [Tooltip("Causes players sprite to change to a different stance.")]
+    public bool TwoHanded;
+}
