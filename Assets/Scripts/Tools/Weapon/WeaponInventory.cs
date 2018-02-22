a27504c7085ac71a5830aba296294332eaeb807e
Myggski <tomaswallin@hotmail.com>
2018-02-22 19:31:38 +0100

    S01E02 - The happening

---
+++ Assets/Scripts/Tools/Weapon/WeaponInventory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WeaponInventory : ScriptableObject
+{
+    public int MaxNumberOfWeapons;
+
+    public Weapon ActiveWeapon;
+
+    public List<Weapon> OwnedWeapons = new List<Weapon>();
+    
+    public void NextWeapon()
+    {
+        var currentIndex = OwnedWeapons.IndexOf(ActiveWeapon);
+        var nextIndex = currentIndex + 1;
+
+        if (nextIndex + 1 > OwnedWeapons.Count)
+            nextIndex = 0;
+
+        ActiveWeapon = OwnedWeapons[nextIndex];
+    }
+
+    public void PrevoiusWeapon()
+    {
+        var currentIndex = OwnedWeapons.IndexOf(ActiveWeapon);
+        var previousIndex = currentIndex - 1;
+
+        if (previousIndex < 0)
+            previousIndex = OwnedWeapons.Count - 1;
+
+        ActiveWeapon = OwnedWeapons[previousIndex];
+    }
+
+    public bool AddWeapon(Weapon weapon)
+    {
+        if(MaxNumberOfWeapons <= OwnedWeapons.Count)
+        {
+            OwnedWeapons.Add(weapon);
+            return true;
+        }
+
+        return false;
+    }
+}
