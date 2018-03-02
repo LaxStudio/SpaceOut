1c547471a71f9b56cb0943f4ce976e6ddf1ed13d
Dennis Sangmo <dennis.sangmo@dark.tempest>
2018-03-02 16:30:57 +0100

    shooter

---
+++ Assets/Scripts/Components/Shooter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(WeaponHolster))]
+public class Shooter : MonoBehaviour {
+
+    private WeaponHolster weaponHolster;
+
+    public KeyCode ShootKeyBinding;
+
+    // Use this for initialization
+    void Start () {
+        weaponHolster = GetComponent<WeaponHolster>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(ShootKeyBinding))
+        {
+            ShootWeapon();
+        }
+    }
+
+    private void ShootWeapon()
+    {
+        Weapon weapon = weaponHolster.WeaponInventory.ActiveWeapon;
+        if(weapon == null)
+        {
+            Debug.Log("No weapon equipped");
+            return;
+        }
+
+        var projectile = weapon.Projectile;
+        Vector3 mouseInWorldCoordinates = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+
+        var projectileTraveler = Instantiate(projectile, transform.position, transform.rotation).GetComponent<Traveler>();
+        projectileTraveler.SetDirectionWithTarget(mouseInWorldCoordinates);
+    }
+}
