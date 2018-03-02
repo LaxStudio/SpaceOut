ad57a5eb67125edc6858d040bfd58619381adbca
MarkuzEriksson <31079019+MarkuzEriksson@users.noreply.github.com>
2018-03-02 11:27:14 +0100

    Added Enemy, Spawner and Waypoints
    Prototype for enemy prefab
    Simple spawner that summons enemies
    Waypoints for the enemy to target

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform enemyPrefab; // Enemy to spawn
+    public float timeBetweenWaves = 5f;
+    public float timeBetweenEnemy = 0.5f;
+
+    private float counter = 2f;
+    private int waveNumber = 1;
+
+    void Update()
+    {
+        if (counter <= 0f)
+        {
+            StartCoroutine( SpawnWave() );
+            counter = timeBetweenWaves;
+        }
+
+        counter -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave ()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenEnemy);
+        }
+        //waveNumber++;
+    }
+
+    void SpawnEnemy ()
+    {
+        Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+
+
+}
