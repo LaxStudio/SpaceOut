16c7a58c95f1ee921e8c3325f6afa7a4bf574b1c
Myggski <tomaswallin@hotmail.com>
2018-03-05 18:01:08 +0100

    Player animation added

--- Assets/Components/KeyboardMover.cs
+++ Assets/Components/KeyboardMover.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 /// Require Component attribute = Adds Rigitbody with black magic when this component is added
 /// </summary>
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class KeyboardMover : MonoBehaviour
 {
     [Serializable]
@@ -29,21 +31,68 @@ public class KeyboardMover : MonoBehaviour
         }
     }
 
+    [Serializable]
+    public class AnimationParameters
+    {
+        public string WalkUpParameterName;
+        public string WalkDownParameterName;
+        public string WalkSidewaysParameterName;
+    }
+
     public FloatVariable MoveRate;
     public MoveAxis Horizontal = new MoveAxis(KeyCode.D, KeyCode.A);
     public MoveAxis Vertical = new MoveAxis(KeyCode.W, KeyCode.S);
+    public AnimationParameters AnimParameters;
+
 
     private Rigidbody2D _rb2d;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
 
     private void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         var moveNormal = new Vector3(Horizontal, Vertical).normalized;
+        SetAnimation();
         var wantedPosition = transform.position + (moveNormal * Time.fixedDeltaTime * MoveRate.Value);
         _rb2d.MovePosition(wantedPosition);
+    }
+
+    private void SetAnimation()
+    {
+
+        _animator.SetBool(AnimParameters.WalkSidewaysParameterName, Horizontal > 0 || Horizontal < 0);
+
+        FlipSpriteChecker();
+
+        if (Vertical > 0)
+        {
+            _animator.SetBool(AnimParameters.WalkUpParameterName, true);
+            _animator.SetBool(AnimParameters.WalkDownParameterName, false);
+        }
+        else if (Vertical < 0)
+        {
+            _animator.SetBool(AnimParameters.WalkUpParameterName, false);
+            _animator.SetBool(AnimParameters.WalkDownParameterName, true);
+        }
+        else
+        {
+            _animator.SetBool(AnimParameters.WalkUpParameterName, false);
+            _animator.SetBool(AnimParameters.WalkDownParameterName, false);
+        }
+    }
+
+    private void FlipSpriteChecker()
+    {
+        if (Horizontal < 0 && !_spriteRenderer.flipX || Horizontal > 0 && _spriteRenderer.flipX)
+            _spriteRenderer.flipX = !_spriteRenderer.flipX;
+        
     }
 }
