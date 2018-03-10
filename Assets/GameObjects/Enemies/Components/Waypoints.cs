2d495189d83164a86d2cdd0d3137e70e7306901a
MarkuzEriksson <31079019+MarkuzEriksson@users.noreply.github.com>
2018-03-10 14:22:01 +0100

    EnemySpawner
    Enemies spawn from a portal and moves to the waypoints

---
+++ Assets/GameObjects/Enemies/Components/Waypoints.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+
+    public static Transform[] points;
+
+    private void Awake()
+    {
+
+        // Create array of all waypoints
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+
+    }
+
+}
